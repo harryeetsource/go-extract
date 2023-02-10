56f7d282355775649e36433414f96e969a4320f5
harryeetsource <95581121+harryeetsource@users.noreply.github.com>
2023-02-10 09:23:25 -0800

    Update PE_dump.go
    modified to not dump files larger than 100mb (avoids FPs), and searches for the MZ executable header instead of PE

--- PE_dump.go
+++ PE_dump.go
@@ -5,6 +5,9 @@ import (
 	"os"
 )
 
+const MB = 1024 * 1024
+const maxFileSize = 100 * MB
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("Usage: extract_pe [filepath]")
@@ -19,6 +22,18 @@ func main() {
 	}
 	defer f.Close()
 
+	fileInfo, err := f.Stat()
+	if err != nil {
+		fmt.Printf("Error getting file info: %v\n", err)
+		os.Exit(1)
+	}
+	fileSize := fileInfo.Size()
+
+	if fileSize > maxFileSize {
+		fmt.Printf("File size is larger than %d MB, skipping\n", maxFileSize/MB)
+		os.Exit(0)
+	}
+
 	var offset int64
 	offset = 0
 	for {
@@ -29,7 +44,7 @@ func main() {
 			break
 		}
 
-		if string(peSignature) == "PE\x00\x00" {
+		if string(peSignature) == "MZ\x90\x00" {
 			peFile, err := os.Create(fmt.Sprintf("%08x.exe", offset))
 			if err != nil {
 				fmt.Printf("Error creating file: %v\n", err)
@@ -37,20 +52,24 @@ func main() {
 			}
 			defer peFile.Close()
 
+			// Determine the size of the PE header
+			peHeaderSize := int64(0)
+			f.Seek(offset+0x3C, 0)
+			f.Read(peSignature)
+			peHeaderSize = int64(peSignature[0]) | int64(peSignature[1])<<8 | int64(peSignature[2])<<16 | int64(peSignature[3])<<24
+
 			_, err = f.Seek(offset, 0)
 			if err != nil {
 				fmt.Printf("Error seeking in file: %v\n", err)
 				break
 			}
 
-			peData := make([]byte, 65536)
-			for {
-				n, err := f.Read(peData)
-				if err != nil || n == 0 {
-					break
-				}
-				peFile.Write(peData[:n])
+			peData := make([]byte, peHeaderSize)
+			n, err := f.Read(peData)
+			if err != nil || n == 0 {
+				break
 			}
+			peFile.Write(peData[:n])
 		}
 		offset++
 	}
